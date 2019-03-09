be139b29cfd3e6541aa34e29b149b403a463f718
kurame <jq1234@live.cn>
2019-03-09 17:37:15 +0800

    3.9

---
+++ WpfApp1/FountainGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FountainSharp;
+using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Collections;
+
+namespace WpfApp1
+{
+    class FountainGame
+    {
+        public FountainDocument Fountain { get; }
+        public FSharpList<FountainBlockElement> Blocks => Fountain.Blocks;
+        public string Html => HtmlFormatter.WriteHtml(Fountain);
+        public List<FountainBlockElement> Action => Blocks.Where((b) => b.IsAction).ToList();
+        public List<FountainBlockElement> Boneyard => Blocks.Where((b) => b.IsBoneyard).ToList();
+        public List<FountainBlockElement> Centered => Blocks.Where((b) => b.IsCentered).ToList();
+        public List<FountainBlockElement> Character => Blocks.Where((b) => b.IsCharacter).ToList();
+        public List<FountainBlockElement> Dialogue => Blocks.Where((b) => b.IsDialogue).ToList();
+        public List<FountainBlockElement> DualDialogue => Blocks.Where((b) => b.IsDualDialogue).ToList();
+        public List<FountainBlockElement> Lyrics => Blocks.Where((b) => b.IsLyrics).ToList();
+        public List<FountainBlockElement> PageBreak => Blocks.Where((b) => b.IsPageBreak).ToList();
+        public List<FountainBlockElement> Parenthetical => Blocks.Where((b) => b.IsParenthetical).ToList();
+        public List<FountainBlockElement> SceneHeading => Blocks.Where((b) => b.IsSceneHeading).ToList();
+        public List<FountainBlockElement> Section => Blocks.Where((b) => b.IsSection).ToList();
+        public List<FountainBlockElement> Synopses => Blocks.Where((b) => b.IsSynopses).ToList();
+        public List<FountainBlockElement> TitlePage => Blocks.Where((b) => b.IsTitlePage).ToList();
+        public List<FountainBlockElement> Transition => Blocks.Where((b) => b.IsTransition).ToList();
+        public FountainGame(string Text)
+        {
+            Fountain = FountainDocument.Parse(Text);
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FountainGame FountainGame;
+        private async Task<FountainGame> FountainGameAsync(string Text) => await Task.Run(() => new FountainGame(Text));
+
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private async void TextEditor_TextChanged(object sender, EventArgs e)
+        {
+            TextEditor text = (TextEditor)sender;
+            FountainGame = await FountainGameAsync(text.Text);
+            webBrowser.NavigateToString(FountainGame.Html);
+            
+
+            #region Auto HighLight Text
+
+
+
+            #endregion
+        }
+    }
+}
