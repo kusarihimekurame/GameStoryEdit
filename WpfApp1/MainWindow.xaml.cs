74d5a6929bfcce4023dda5eac198d7a05a043720
kurame <jq1234@live.cn>
2019-03-09 22:02:22 +0800

    3.9

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html;
+using iTextSharp.tool.xml;
+using System;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using ICSharpCode.AvalonEdit;
-using ICSharpCode.AvalonEdit.Highlighting;
 
 namespace WpfApp1
 {
@@ -21,7 +26,7 @@ namespace WpfApp1
 
         public MainWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();
         }
 
         private async void TextEditor_TextChanged(object sender, EventArgs e)
@@ -29,11 +34,27 @@ namespace WpfApp1
             TextEditor text = (TextEditor)sender;
             FountainGame = await FountainGameAsync(text.Text);
             webBrowser.NavigateToString(FountainGame.Html);
-            
+            //HTMLToPdf(FountainGame.Html, @"F:\GameStory.pdf");
 
             #region Auto HighLight Text
 
 
+
+            #endregion
+        }
+
+        public void HTMLToPdf(string HTML, string FilePath)
+        {
+            #region HTML to Pdf
+
+            Document document = new Document();
+            TextReader textReader=new StringReader(HTML);
+            PdfWriter pdfWriter = PdfWriter.GetInstance(document, new FileStream(FilePath, FileMode.Create));
+            document.Open();
+            XMLWorkerHelper.GetInstance().ParseXHtml(pdfWriter, document, textReader);
+            document.Close();
+            pdfWriter.Close();
+            textReader.Close();
 
             #endregion
         }
